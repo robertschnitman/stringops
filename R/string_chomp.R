9162c18a6fec7fa2b7c0d57ff7dbd35eb02e5d83
Robert Schnitman <robertschnitman@gmail.com>
2020-07-31 18:29:46 -0500

    New functions: string_trim() and %like%

--- R/string_chomp.R
+++ R/string_chomp.R
@@ -1,7 +1,40 @@
-string_chomp <- function(s) gsub(" |\n|\t", "", s)
+string_chomp <- function(s) gsub(" |\n|\t|\r", "", s)
 s_chomp      <- string_chomp
 chomp        <- string_chomp
 
 string_chop <- function(s) substr(s, 1, nchar(s) - 1)
 s_chop      <- string_chop
 chop        <- string_chop
+
+string_trim <- function(s, which = c('both', 'left', 'right'), whitespace = '[ \t\r\n]') {
+  
+  # Make sure arguments match "which".
+  which <- match.arg(which)
+  
+  # Define functions for easier referencing.
+  ## Remove specific patterns
+  sub_out    <- function(x, r) gsub(r, '', x, perl = TRUE)
+  
+  ## vectorize switch so that we can apply the substitutions in a vectorized manner. 
+  switch_out <- function(x, ...) sapply(x, switch, ...)
+  
+  # Conditions for each "which" input.
+  cond_beg  <- '^' %&% whitespace %&% '+'
+  cond_end  <- whitespace %&% '+$'
+  cond_both <- "^" %&% whitespace %&% "|" %&% whitespace %&% "+$"
+  
+  # Output should be based on user input for "which".
+  output <- switch_out(which,
+                       left  = sub_out(s, cond_beg),
+                       right = sub_out(s, cond_end),
+                       both  = sub_out(s, cond_both))
+  
+  # Initial output is a matrix, so flatten it to a vector so that 
+  # users can use string_trim() on dataframe columns, for example.
+  output <- as.vector(output)
+  
+  output
+  
+}
+s_trim      <- string_trim
+trim        <- string_trim
--- R/string_compare.R
+++ R/string_compare.R
@@ -1,3 +1,5 @@
-string_isblank <- function(s) ifelse(s == "" | grepl(" *", s), TRUE, FALSE)
+string_isblank <- function(s) trimws(s) == ""
 s_isblank      <- string_isblank
 isblank        <- string_isblank
+
+`%like%` <- function(s, pattern) grepl(pattern, s)
