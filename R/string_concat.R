dec83a27cfe98736bb7666387ae2d3987bf79748
Robert Schnitman <robertschnitman@gmail.com>
2020-08-09 14:51:02 -0500

    string_flag() is now scalar; new function: string_flagv()

--- R/string_concat.R
+++ R/string_concat.R
@@ -12,7 +12,11 @@
 #' suffix(s, suffix)
 #' string_flag(s, flag, width)
 #' s_flag(s, flag, width)
-#' flag(s, flag, width)
+#' flag(s, flag, width, which = c('left', 'right', 'both'))
+#' string_flagv(s, ...)
+#' s_flagv(s, ...)
+#' flagv(s, ...)
+#' 
 #'
 #' @param s A string (character) vector.
 #' @param a A string (character) vector.
@@ -20,6 +24,8 @@
 #' @param collapse A string by which to separate the elements of a vector.
 #' @param flag Character or number to append to a string.
 #' @param width Number of characters a string should be.
+#' @param which Side to flag a string.
+#' @param ... Parameters passed to \code{string_flag()}.
 #' 
 #' @return Character vector.
 #' 
@@ -27,7 +33,9 @@
 #' 
 #' \code{string_prefix()} and its synonyms prefix a string to a vector, while \code{string_suffix()} and its synonyms suffix a string to a vector.
 #' 
-#' \code{string_flag()} appends a character or number to a string if it does not meet a specified width.
+#' \code{string_flag()} is a scalar function that appends a character or number to a string if it does not meet a specified width.
+#' 
+#' \code{string_flagv()} is a vectorized version of \code{string_flag()}.
 #'
 #' @examples
 #' "a" %&% "b"
@@ -54,10 +62,50 @@ s_suffix      <- string_suffix
 suffix        <- string_suffix
 
 #' @rdname string_flag
-string_flag <- function(s, flag, width) {
+string_flag <- function(s, flag, width, which = c('left', 'right', 'both')) {
   
-  ifelse(string_len(s) != width, flag %&% s, s) 
+  # string_flag() is a scalar function
   
-  #formatC(x = s, width = width, flag = flag)
+  # Check which input
+  which <- match.arg(which)
+  
+  # Repeat flag a specified number of times.
+  diff      <- width - nchar(s)
+  diff_adj  <- ifelse(diff <= 0, 1, diff)
+  
+  flags <- string_join(rep(flag, diff_adj))
+  
+  flags <- ifelse(nchar(s) > width,
+                  string_remove(flags, flags),
+                  flags)
+  
+  # Special case for "both".
+  if (which == 'both') {
+    
+    flags <- list(l = string_join(rep(flag, diff_adj/2)), 
+                  r = string_join(rep(flag, diff_adj/2)))
+    
+  }
+  
+  # Output should be a string vector based on the "which" input.
+  output <- switch(which,
+                   left = flags %&% s,
+                   right = s %&% flags,
+                   both = flags$l %&% s %&% flags$r)
+  
+  output
   
 }
+
+#' @rdname string_flagv
+string_flagv <- function(s, ...) {
+  
+  output <- sapply(s, function(x) string_flag(x, ...))
+  
+  names(output) <- NULL
+  
+  output
+  
+}
+s_flagv <- string_flagv
+flagv   <- string_flagv
